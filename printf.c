95a2f3ad6da3b087405faab6b63350547f6e857d
abdellah34 <aammari.1989@gmail.com>
2023-08-21 13:35:47 -0700

    update printf

--- printf.c
+++ printf.c
@@ -1,14 +1,33 @@
 #include "main.h"
 /**
- * print_str - writes the string str to stdout
+ * print_str - to write the string str to stdout
  * @str: The string to print
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately.
+ * Return:the count of characters.
  **/
 int print_str(char *str)
 {
-		return (write(1, str, strlen(str)));
+	int count = 0;
+
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+		count++;
+	}
+	return (count);
 }
+
+/**
+ * print_char - to write the character.
+ * @c: The charactr to print
+ * Return: 1.
+ **/
+int print_char(char c)
+{
+        putchar(c);
+        return (1);
+}
+
 /**
  * _printf - converts formats, and prints its arguments .
  * @format: the format of string
@@ -17,7 +36,8 @@ int print_str(char *str)
 int _printf(const char *format, ...)
 {
 	int ch_print = 0;
-	char c, *str;
+	int c;
+	char *str;
 	va_list ls_args;
 
 	if (format == NULL)
@@ -28,8 +48,7 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			ch_print++;
+			ch_print += print_char(*format);
 		}
 		else
 		{
@@ -40,20 +59,16 @@ int _printf(const char *format, ...)
 			}
 			if (*format == '%')
 			{
-				write(1, format, 1);
-				ch_print++;
+				ch_print += print_char('%');
 			}
-			else if (*format == 'c')
-			{
+			else if (*format == 'c'){
 				c = va_arg(ls_args, int);
-				write(1, &c, 1);
-				ch_print++;
+				ch_print += print_char(c);
 			}
 			else if (*format == 's')
 			{
 				str = va_arg(ls_args, char*);
-				print_str(str);
-				ch_print += strlen(str);
+				ch_print += print_str(str);
 			}
 		}
 		format++;
