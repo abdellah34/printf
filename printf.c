937657d9ead389e9da3f8b6661104434300124d1
brahim aammari <brahimaa.494@gmail.com>
2023-08-21 13:01:47 -0700

    task0

--- printf.c
+++ printf.c
@@ -10,6 +10,30 @@ int print_str(char *str)
 		return (write(1, str, strlen(str)));
 }
 /**
+ * null_format - writes the string str to stdout
+ * @str: The string to print
+ * Return: On success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ **/
+int null_format(const char *str)
+{
+	if (str == NULL)
+		return (-1);
+	return (0);
+}
+/**
+ * print_notpercent - writes the string str to stdout
+ * @str: The string to print
+ * Return: On success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ **/
+int print_notpercent(const char *str)
+{
+	if (*str != '%')
+		return (write(1, str, 1));
+	return (0);
+}
+/**
  * _printf - converts formats, and prints its arguments .
  * @format: the format of string
  * Return: the number of characters printed.
@@ -20,17 +44,12 @@ int _printf(const char *format, ...)
 	char c, *str;
 	va_list ls_args;
 
-	if (format == NULL)
-		return (-1);
-
+	null_format(format);
 	va_start(ls_args, format);
 	while (*format)
 	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
+		if (print_notpercent(format))
 			ch_print++;
-		}
 		else
 		{
 			format++;
