98289908445aefd1ba62e583e1c0500eecb07d0c
abdellah34 <aammari.1989@gmail.com>
2023-08-22 09:28:02 -0700

    update printf

--- printf.c
+++ printf.c
@@ -1,60 +1,30 @@
 #include "main.h"
 /**
- * print_str - writes the string str to stdout
+ * print_str - to write the string str to stdout
  * @str: The string to print
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately.
+ * Return:the count of characters.
  **/
 int print_str(char *str)
 {
-		return (write(1, str, strlen(str)));
-}
-/**
- * null_format - writes the string str to stdout
- * @str: The string to print
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately.
- **/
-int null_format(const char *str)
-{
-	if (str == NULL)
-		return (-1);
-	return (0);
-}
-/**
- * print_notpercent - writes the string str to stdout
- * @str: The string to print
- * @c : 2 param
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately.
- **/
-int print_notpercent(const char *str, int *c)
-{
-	if (*str != '%')
+	int count = 0;
+
+	while (*str)
 	{
-		c++;
-		return (write(1, str, 1));
+		putchar(*str);
+		str++;
+		count++;
 	}
-	return (0);
+	return (count);
 }
 /**
- * print_percent - writes the string str to stdout
- * @str: The string to print
- * @c : 2 param
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately.
+ * print_char - to write the character.
+ * @c: The charactr to print
+ * Return: 1.
  **/
-int print_percent(const char *str, int *c)
+int print_char(char c)
 {
-	str++;
-	if (*str == '\0')
-		return (-1);
-	if (*str == '%')
-	{
-		c++;
-		return (write(1, str, 1));
-	}
-	return (0);
+	putchar(c);
+	return (1);
 }
 /**
  * _printf - converts formats, and prints its arguments .
@@ -63,35 +33,35 @@ int print_percent(const char *str, int *c)
  */
 int _printf(const char *format, ...)
 {
-	int ch_count = 0;
-	char c, *str;
+	int ch_print = 0;
+
 	va_list ls_args;
 
-	null_format(format);
+	if (format == NULL)
+		return (-1);
 	va_start(ls_args, format);
 	while (*format)
 	{
-		print_notpercent(format, &ch_count);
-		if (*format == '%')
+		if (*format != '%')
 		{
-			print_percent(format, &ch_count);
-			if (*format == 'c')
-			{
-				c = va_arg(ls_args, int);
-				write(1, &c, 1);
-				ch_count++;
-			}
+			ch_print += print_char(*format);
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+				ch_print += print_char('%');
+			else if (*format == '!')
+				ch_print += print_str("%!");
+			else if (*format == 'c')
+				ch_print += print_char(va_arg(ls_args, int));
 			else if (*format == 's')
-			{
-				str = va_arg(ls_args, char*);
-				print_str(str);
-				ch_count += strlen(str);
-			}
-			else
-				write(1, format, 1);
+				ch_print += print_str(va_arg(ls_args, char*));
 		}
 		format++;
 	}
 	va_end(ls_args);
-	return (ch_count);
+	return (ch_print);
 }
